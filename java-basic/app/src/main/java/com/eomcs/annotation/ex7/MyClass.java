acf60919613cc483133a9beaa26a1f3cdf6c265d
chaerin <chaexxrin@gmail.com>
2024-01-10 18:16:08 +0900

    .

--- java-basic/app/src/main/java/com/eomcs/annotation/ex7/MyClass.java
+++ java-basic/app/src/main/java/com/eomcs/annotation/ex7/MyClass.java
@@ -2,14 +2,14 @@
 package com.eomcs.annotation.ex7;
 
 @Company("비트캠프")
-//@Company("비트캠프") // 중복 사용 불가!
+// @Company("비트캠프") // 중복 사용 불가!
 public class MyClass {
 
   @Company("비트캠프")
-  //@Company("비트캠프") // 중복 사용 불가!
+  // @Company("비트캠프") // 중복 사용 불가!
   public void m1(int p) {}
 
   @Company("비트캠프")
-  //@Company("비트캠프") // 중복 사용 불가!
+  // @Company("비트캠프") // 중복 사용 불가!
   public void m2(int p) {}
 }
