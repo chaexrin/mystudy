ff74d3519670b6f3652f5df8a6bc826e6d201f0e
Chaerin <chaexxrin@gmail.com>
2024-03-20 09:40:28 +0900

    요청핸들러

--- mvc-app/app/src/main/java/bitcamp/config/App2Config.java
+++ mvc-app/app/src/main/java/bitcamp/config/App2Config.java
@@ -1,10 +1,42 @@
 package bitcamp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.util.UrlPathHelper;
 
+@EnableWebMvc
 @ComponentScan(value = "bitcamp.app2")
-public class App2Config {
+public class App2Config implements WebMvcConfigurer {
 
+    // DispatcherServlet의 기본 ViewResolver를 교체하기
+    // 1) XML 설정
+    // <bean id="viewResolver"
+    //       class="org.springframework.web.servlet.view.InternalResourceViewResolver">
+    //   <property name="viewClass" value="org.springframework.web.servlet.view.JstlView"/>
+    //   <property name="prefix" value="/WEB-INF/jsp/"/>
+    //   <property name="suffix" value=".jsp"/>
+    // </bean>
+    //
+    // 2) Java Config 설정
+    @Bean
+   public ViewResolver viewResolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver(
+            "/WEB-INF/jsp/",
+            ".jsp"
+        );
+        return vr;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
 }
