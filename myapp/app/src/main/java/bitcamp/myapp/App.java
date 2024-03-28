20d424542cbe9235ce27779310bf89ac5d3611bd
Chaerin <chaexxrin@gmail.com>
2024-03-28 16:26:39 +0900

    myapp#87-springboot-2

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -2,12 +2,32 @@ package bitcamp.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Configuration
+@EnableTransactionManagement
+@PropertySource({
+    "classpath:config/ncp.properties",
+    "classpath:config/ncp-secret.properties"})
+@Controller // GetMapping 인식 가능
 public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("과제관리 시스템 서버 실행!");
         SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/home")
+    public void home() {
+        // return "home"; // ==> ThymeleafViewResolver가 처리한다.
+    }
+
+    @GetMapping("/about")
+    public void about() {
     }
 }
