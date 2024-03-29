de1627edb6b8c90a0e7a78f2cd3e82c8db10a9de
Chaerin <chaexxrin@gmail.com>
2024-03-29 09:38:04 +0900

    sqlstudy

--- dining/app/doc/sqlstudy.sql
+++ dining/app/doc/sqlstudy.sql
@@ -9,7 +9,7 @@
 SHOW DATABASES;
 
 -- 데이터 베이스 삭제
-DROP DATABASE company;
+DROP DATABASE if exists company;
 
 -- 현재 어떤 db 사용중인지 조회
 SELECT database();
@@ -182,3 +182,68 @@ from employee;
 -- 1. SELECT로 조회할 때 조건들을 포함해서 조회를 한다면
 -- 해당 조건들과 관련된 attributes에 index가 걸려있어야 한다.
 -- 그렇지 않다면 데이터가 많아질수록 조회속도가 느려진다.
+
+-- <서브쿼리>
+-- ID가 14인 임직원보다 생일이 빠른 임직원의 ID, 이름, 생일을 알고싶을 때
+
+select birth_date from employee where id = 14;
+
+select id, name, birth_date from employee
+   where birth_date < '1993-05-13';
+
+-- 위의 쿼리를 합쳤을 때 
+-- >>>
+select id, name, birth_date from employee
+where birth_date < (select birth_date from employee where id = 14);
+
+-- subquery : (nested query or inner query); SELECT, INSERT, UPDATE, DELETE 에 포함된 query
+-- outer query(main query): subquery를 포함하는 query
+-- subquery는 ()안에 기술된다.
+
+-- ID 가 1인 임직원과 같은 부서 같은 성별인 임직원들의 ID와 이름과 직군을 알고싶다
+SELECT id, name, position
+from employee
+where (dept_id, sex) = (
+    SELECT dept_id, sex
+    from employee
+    WHERE id = 1
+);
+
+-- ID가 5인 임직원과 같은 프로젝트에 참여한 임직원들의 Id를 알고싶다.
+SELECT proj_id from WORKS_ON where empl_id = 5;
+
+SELECT DISTINCT empl_id from WORKS_ON
+where empl_id != 5 and (proj_id = 2001 or proj_id = 2002);
+
+-- (proj_id = 2001 or proj_id = 2002) = proj_id IN(2001, 2002)
+
+-- 위의 두 쿼리를 합쳤을 때
+Select DISTINCT empl_id from WORKS_ON
+where empl_id != 5 and proj_id in ( select proj_id from WORKS_ON where empl_id = 5);
+
+-- unqualified attribute가 참조하는 table은 해당 attribute가 사용된 query를 포함하여
+-- 그 query의 바깥쪽으로 존재하는 모든 queries 중에 해당 attribute 이름을 가지는 가장 가까이에 있는 table을 참조한다.
+
+---- ID가 5인 임직원과 같은 프로젝트에 참여한 임직원들의 Id와 이름을 알고싶다.
+SELECT id, name 
+FROM employee , 
+(
+    select DISTINCT empl_id from WORKS_ON
+where empl_id != 5 and proj_id in ( select proj_id from WORKS_ON where empl_id = 5)
+) AS DISTINCT_E
+where id = DISTINCT_E.empl_id;
+
+-- ID가 7 혹은 12인 임직원이 참여한 프로젝트의 ID와 이름을 알고싶다
+SELECT P.id, P.name
+FROM project p
+WHERE EXISTS (
+    SELECT *
+    FROM WORKS_ON W 
+    WHERE W.proj_id = P.id AND W.empl_id IN (7,12)
+);
+
+-- correlated query: subquery가 바깥쪽 query의 attribute를 참조할 때, correlated subquery라 부름
+
+-- EXISTS: subquery의 결과가 최소 하나의 row라도 있다면 TRUE를 반환
+
+-- NOT EXISTS: subquery의 결과가 단 하나의 row도 없다면 TRUE를 반환
